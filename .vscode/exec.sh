214a4c77ead4192e001be31b3071561a07bb8482
Mr_Tree <945962873@qq.com>
2021-04-10 22:08:40 +0800

    正常版本，编译耗时 649.36ms

--- .vscode/exec.sh
+++ .vscode/exec.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+# compile
+rm -r ../build
+if [ ! -d "./build" ]; then
+    mkdir ../build
+fi
+cd ../build
+cmake ..
+make
 
-# rm -r ../build
-# if [ ! -d "./build" ]; then
-#     mkdir ../build
-# fi
-
-# cd ../build
-# time cmake ..
-# make
-
+# time trace
 cd ../build/src/CMakeFiles/c_compiler.dir
 file_count=0
-echo "json file:"
+
+echo "$ current dir: "$(pwd)
+echo "$ json file:"
 for file_name in $(ls *.json)
 do
     echo ' '$file_name
@@ -20,6 +22,13 @@ do
 done
 json_path=../build/src/CMakeFiles/c_compiler.dir
 cd ../../../../.vscode
-python time_trace.py $json_path/${json_file[0]} $json_path/${json_file[1]}
-# cd src
-# ./c_compiler
+
+if [ $file_count -eq 1 ]; then
+    python time_trace.py $json_path/${json_file[0]}
+else
+    python time_trace.py $json_path/${json_file[0]} $json_path/${json_file[1]}
+fi
+
+# run
+cd ../build/src/
+./c_compiler
---
+++ README.md
@@ -0,0 +1,15 @@
+# c_compiler
+
+编译耗时分析
+
+### master 
+
+--分支为正常业务代码
+
+### dev_unity_build 
+
+--使用unity build进行编译时间优化
+
+### dev_cotire 
+
+使用cotire加速
