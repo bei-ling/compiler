981196b01526355feebd0360adc6c301454bb7b0
tree <945962873@qq.com>
2022-04-06 22:32:30 +0800

    修改为Ninja构建

--- build.py
+++ build.py
@@ -6,8 +6,8 @@ base_path = os.path.dirname(os.path.abspath(__file__))
 def build():
     os.chdir("{}/build".format(base_path))
 
-    os.system("cmake ..")
-    ret = os.system("make -j4")
+    os.system("cmake .. -G 'Ninja'")
+    ret = os.system("ninja -j4")
     if ret != 0:
         return
     os.chdir(base_path)
